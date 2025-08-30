0cdd6699c17b23ae39fdad037c55cb33b97079f3
kjking11-android <kojiking5454@gmail.com>
2025-08-31 00:11:27 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
+const CACHE_NAME = 'm3webtest-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles/main.css',
-  '/scripts/main.js'
+  '/m3webtest/',
+  '/m3webtest/index.html',
+  '/m3webtest/app.js',
+  '/m3webtest/icons/icon-192x192.png',
+  '/m3webtest/icons/icon-512x512.png',
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -19,10 +21,7 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+        return response || fetch(event.request);
       })
   );
 });
